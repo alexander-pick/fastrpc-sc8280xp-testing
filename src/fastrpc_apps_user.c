alexander-pick/fastrpc-sc8280xp-testing

commit 7797ba9cdbbbdb4a34e2b5d8fe4568ea94f26e4f

apps_user: fix memory leak
memory passed to the ioctl must be deallocated on use if no longer
required

Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>

src/fastrpc_apps_user.c
@@ -795,6 +795,7 @@ int remote_handle_invoke_domain(int domain, remote_handle handle, uint32_t sc, r
     FARF(HIGH,"debug:sc:%x,handle:%x\n",sc,handle);
 	nErr = ioctl(dev, req, (unsigned long)&invoke);
 bail:
+	free(args);
 	return nErr;
 }
 

commit 740cae0997103fafb58c4f3c73f419ba45f8c2f6

fastrpc: apps_user: check args before freeing.
args pointer can be uninitalized before calling free.
Code path for this is via VERIFYC macro jumping to bail label
which is not very explicit from the code.

Fixes: 7797ba9cdbbb (apps_user: fix memory leak)
Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

src/fastrpc_apps_user.c
@@ -748,7 +748,7 @@ static int fdlist_fd_to_buf(void *buf)
 
 int remote_handle_invoke_domain(int domain, remote_handle handle, uint32_t sc, remote_arg* pra) {
     struct fastrpc_invoke invoke;
-	struct fastrpc_invoke_args *args;
+	struct fastrpc_invoke_args *args = NULL;
 	int bufs, i, req, nErr = 0;
 	int dev;
 	VERIFY(dev != -1);
@@ -795,7 +795,9 @@ int remote_handle_invoke_domain(int domain, remote_handle handle, uint32_t sc, r
     FARF(HIGH,"debug:sc:%x,handle:%x\n",sc,handle);
 	nErr = ioctl(dev, req, (unsigned long)&invoke);
 bail:
-	free(args);
+	if (args)
+		free(args);
+
 	return nErr;
 }
 